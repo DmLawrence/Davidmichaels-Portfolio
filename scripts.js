23956049f218a9fbc81c5e2bde7071ad47b09002
DmLawrence <dmlawrence925@gmail.com>
2023-04-03 19:38:07 -0400

    Fixed vertical resizing and scroll lock on fullscreen images

--- scripts.js
+++ scripts.js
@@ -5,5 +5,26 @@ imgs.forEach(img => {
   img.addEventListener('click', function() {
     fullPage.style.backgroundImage = 'url(' + img.src + ')';
     fullPage.style.display = 'block';
+    scrollDisable();
   });
 });
+
+function closeFullscreen() {
+  fullPage.style.display='none';
+  scrollEnable();
+}
+
+function scrollDisable() {
+    // To get the scroll position of current webpage
+    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+    LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
+
+    // if scroll happens, set it to the previous value
+    window.onscroll = function() {
+    window.scrollTo(LeftScroll, TopScroll);
+            };
+    }
+
+function scrollEnable() {
+window.onscroll = function() {};
+}
