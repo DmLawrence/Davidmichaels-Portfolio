16b1c152acfa16a05a673e9cb8e2d66cadfb240b
DmLawrence <dmlawrence925@gmail.com>
2023-04-07 13:02:27 -0400

    added like and favorite functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,31 @@
-const imgs = document.querySelectorAll('.gallery img');
+//Like button function
+const like = document.querySelectorAll('.vote img');
+
+like.forEach(like => {
+  like.addEventListener('click', function(){
+   if ( like.src.indexOf('like.png') === -1){
+      like.src = `resources/like.png`
+    } else {
+      like.src = `resources/like-black.png`
+    }
+  });
+});
+
+//Favorite button function
+const fav = document.querySelectorAll('.favorite img');
+
+fav.forEach(fav => {
+  fav.addEventListener('click', function(){
+   if (fav.src.indexOf('fav.png') === -1){
+      fav.src = `resources/fav.png`;
+    } else {
+      fav.src = `resources/fav-black.png`;
+    }
+  });
+});
+
+//Makes gallery images full screen
+const imgs = document.querySelectorAll('.gallery-img img');
 const fullPage = document.querySelector('#fullpage');
 
 imgs.forEach(img => {
